bb505297e794639834fac9dfa2dbf92c9188792f
nitinpainoli <44173381+nitinpainoli@users.noreply.github.com>
2023-02-14 19:20:17 +0530

    Update my.groovy

--- vars/my.groovy
+++ vars/my.groovy
@@ -1,76 +1,114 @@
-def call(String agent, String TERRAFORM_DIR) {
-	
-	
-pipeline {
+
+def call(Map args = [
+                      dir: '.',
+                      agent: ''
+                     ] ){
+
+
+  args.agent =  args.agent ?: '.'
+  args.dir = args.dir ?: '.'
+  
+
+  pipeline {
+
     agent {
-    label "${agent}"
-  }
+      label ("$agent")
+    }
+
+    environment {
+      TERRAFORM_DIR = "$args.dir"
+    }
+
     stages {
-	    
-	    
-     	stage("Clean Workspace") {
-				steps {
-					cleanWs()
-				}
-			}
-   
-        stage("Terraform init") {
-		  steps {
-			  dir("${TERRAFORM_DIR}") {
+
+        stage('Terraform Fmt') {
+          steps {
+                dir("${TERRAFORM_DIR}") {  
+                sh "terraform fmt -list=true -diff=true"
+		 }
+          }
+        }
+  
+
+      stage('Terraform Init') {
+        steps {
+        dir("${TERRAFORM_DIR}") {
 
 			           sh "ls -la"
 				    sh "terraform init -input=false"	
 				}
-			  
-			} 
-	     }
-				
-        stage('Terraform-Format') {
-            steps {
-		 dir("${TERRAFORM_DIR}") {  
-                sh "terraform fmt -list=true -diff=true"
-		 }
-            }
-            }
+        }
+      }
 
-        stage('Terraform-Validate') {
-            steps {
-	        dir("${TERRAFORM_DIR}") {    
+      stage('Terraform Validate') {
+        steps {
+          dir("${TERRAFORM_DIR}") {    
                 sh "terraform validate"
 		}
-            }
-            }
+        }
+      }
 
-        stage('Terraform-Plan') {
-            steps {
-		dir("${TERRAFORM_DIR}") {	    
+      stage('Terraform Plan') {
+        steps {
+         dir("${TERRAFORM_DIR}") {	    
                 sh 'terraform plan -out tfplan'
 		}
-            }
-            }
-
-        stage('Terraform-Approval') {
-            steps {
-	        dir("${TERRAFORM_DIR}") {		    
-                script {
-                timeout(time: 10, unit: 'MINUTES') {
-                    def userInput = input(id: 'Approve', message: 'Do You Want To Apply The Terraform Changes?', parameters: [
-                    [$class: 'BooleanParameterDefinition', defaultValue: false, description: 'Apply Terraform Changes', name: 'Approve?']
-                    ])
-                }
-                }
-            }
-            }
-	}
-
-//         stage('Terraform-Apply') {
-//             steps {
-//                 sh 'terraform apply -input=false tfplan'
-                
-//             }
-//             }
-
         }
-}	
-}
+      }
 
+    //   stage('Approval') {
+    //     when {
+    //       beforeInput true
+    //       allOf {
+    //         expression {
+    //           env.TERRAFORM_CHANGES != 'false'
+    //         }
+    //         anyOf {
+    //           // XXX: branch pattern fails to match anything unless part of a multibranch pipeline
+    //           branch pattern: "$args.apply_branch_pattern", comparator: "REGEXP"
+    //           // which is why we use an expression evaluation here
+    //           expression {
+    //             (env.GIT_BRANCH =~ ~"$args.apply_branch_pattern").matches()
+    //           }
+    //         }
+    //       }
+    //     }
+    //     steps {
+    //       //approval(args.approval_args)
+    //       withEnv(args.env){
+    //         approval()
+    //       }
+    //     }
+    //   }
+
+    //   stage('Terraform Apply') {
+    //     when {
+    //       allOf {
+    //         expression {
+    //           env.TERRAFORM_CHANGES != 'false'
+    //         }
+    //         anyOf {
+    //           // XXX: branch pattern fails to match anything unless part of a multibranch pipeline
+    //           branch pattern: "$args.apply_branch_pattern", comparator: "REGEXP"
+    //           // which is why we use an expression evaluation here
+    //           expression {
+    //             (env.GIT_BRANCH =~ ~"$args.apply_branch_pattern").matches()
+    //           }
+    //         }
+    //       }
+    //     }
+    //     steps {
+    //       withEnv(args.env){
+    //         withCredentials(args.creds){
+    //           terraformApply()
+    //         }
+    //       }
+    //     }
+    //   }
+
+    }
+
+
+  }
+
+}
