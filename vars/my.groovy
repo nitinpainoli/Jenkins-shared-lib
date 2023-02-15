98e7cda694b4cd34117d9114ffaaf590c653c3d1
nitinpainoli <44173381+nitinpainoli@users.noreply.github.com>
2023-02-15 12:48:13 +0530

    Update my.groovy

--- vars/my.groovy
+++ vars/my.groovy
@@ -1,91 +1,87 @@
-    def call(Map args =[ jenkins_agent: '', tf_dir: '', tf_workspace: ''] ){
+def call(Map args =[ jenkins_agent: '', tf_dir: '', tf_workspace: ''] ){
 
-        args.tf_dir = args.tf_dir ?: '.'
-        args.jenkins_agent = args.jenkins_agent ?: 'master'
-        args.tf_workspace = args.tf_workspace ?: 'default'	
-    
-    pipeline {
-        agent {
-            label "${args.jenkins_agent}"
-        }
-    
-        environment {
-            TERRAFORM_DIR = "$args.tf_dir"
-            TERRAFORM_WORKSPACE = "$args.tf_workspace"
-        }	
-        
-        stages {
-            
-            
-            stage("Clean Workspace") {
-                    steps {
-                        cleanWs()
-                    }
-                }
-        
-            stage("Terraform init") {
-                steps {
-                    dir("${TERRAFORM_DIR}") {
-    
-                            sh "ls -la"
-                        sh "terraform init -input=false"	
-                    }
-                    
-                } 
-            }
-                    
-            stage('Terraform-Format') {
-                steps {
-            dir("${TERRAFORM_DIR}") {  
-                    sh "terraform fmt -list=true -diff=true"
-            }
-                }
-                }
-    
-            stage('Terraform-Validate') {
-                steps {
-                dir("${TERRAFORM_DIR}") {    
-                    sh "terraform validate"
-            }
-                }
-                }
-    
-            stage('Terraform-Plan') {
-                steps {
-            dir("${TERRAFORM_DIR}") {	    
-                    sh 'terraform plan -out tfplan'
-            }
-                }
-                }
-    
-            stage('Approval') {
-                    when {
-                    beforeInput true
-                    allOf {
-                        expression {
-                          {"${TERRAFORM_WORKSPACE}" != 'prod'}
-                        {
-                            echo "Excecuted only on prod workspace"
+    args.tf_dir = args.tf_dir ?: '.'
+    args.jenkins_agent = args.jenkins_agent ?: 'master'
+    args.tf_workspace = args.tf_workspace ?: 'default'	
 
-                        }
-                        }
-                
-                    }
-                    }
-                    steps {
-                        //dir("${TERRAFORM_DIR}") {		    
-                            script {
-                            timeout(time: 10, unit: 'MINUTES') {
-                                def userInput = input(id: 'Approve', message: 'Do You Want To Apply The Terraform Changes?', parameters: [
-                                [$class: 'BooleanParameterDefinition', defaultValue: false, description: 'Apply Terraform Changes', name: 'Approve?']
-                                ])
-                            }
-                            }
-                        }    
+pipeline {
+    agent {
+        label "${args.jenkins_agent}"
+    }
+
+    environment {
+        TERRAFORM_DIR = "$args.tf_dir"
+        TERRAFORM_WORKSPACE = "$args.tf_workspace"
+    }	
+    
+    stages {
+        
+        
+        stage("Clean Workspace") {
+                steps {
+                    cleanWs()
+                }
+            }
+    
+        stage("Terraform init") {
+            steps {
+                dir("${TERRAFORM_DIR}") {
+
+                        sh "ls -la"
+                    sh "terraform init -input=false"	
                 }
                 
-
-                }
+            } 
         }
-    }  
-    
+                
+        stage('Terraform-Format') {
+            steps {
+        dir("${TERRAFORM_DIR}") {  
+                sh "terraform fmt -list=true -diff=true"
+        }
+            }
+            }
+
+        stage('Terraform-Validate') {
+            steps {
+            dir("${TERRAFORM_DIR}") {    
+                sh "terraform validate"
+        }
+            }
+            }
+
+        stage('Terraform-Plan') {
+            steps {
+        dir("${TERRAFORM_DIR}") {	    
+                sh 'terraform plan -out tfplan'
+        }
+            }
+            }
+
+        stage('Approval') {
+                when {
+                    expression { "${TERRAFORM_WORKSPACE}" != 'prod'}
+                    // {
+                    //     echo "Excecuted only on prod workspace"
+
+                    // }
+                    
+            
+                }
+                
+                steps {
+                    //dir("${TERRAFORM_DIR}") {		    
+                        script {
+                        timeout(time: 10, unit: 'MINUTES') {
+                            def userInput = input(id: 'Approve', message: 'Do You Want To Apply The Terraform Changes?', parameters: [
+                            [$class: 'BooleanParameterDefinition', defaultValue: false, description: 'Apply Terraform Changes', name: 'Approve?']
+                            ])
+                        }
+                        }
+                    }    
+            }
+            
+
+            }
+    }
+}  
